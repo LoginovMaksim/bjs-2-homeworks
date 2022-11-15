157f45a8619118d6dfde275a130a6b96c674fefa
Maksim L <rezerv@net.com>
2022-11-15 04:10:43 +0400

    Nax

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
+  
   // код для задачи №1 писать здесь
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    let sqr = -b / (2 * a);
+    arr = [sqr];
+  } else {
+    let sqr1 = (-b + Math.sqrt(d) ) / (2 * a);
+    let sqr2 = (-b - Math.sqrt(d) ) / (2 * a);
+    arr = [sqr1, sqr2];
+  }
+
   return arr; // array
 }
 
@@ -8,6 +24,21 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
-
+  if (isFinite(percent) === false) {
+    totalAmount= `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isFinite(contribution) === false) {
+    totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isFinite(amount) === false) {
+    totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+  else {
+    let s = amount - contribution;
+    let dateNew = (new Date());
+    let p = (percent/100) / 12; 
+    let n = Math.round((date - dateNew)/2629746000);
+    let Payment = s * (p + (p / (((1 + p)**n) - 1)));
+    totalAmount = Number((Payment*n).toFixed(2));
+  };
+  
   return totalAmount;
 }
